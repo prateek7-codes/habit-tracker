f2746f68112443c52993e3ca932f943ced1cffc4
prateek7-codes <prateeksinha7@gmail.com>
2026-02-23 23:11:32 +0530

    Improve background design

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://bbvdctpmyhsydnpseuvz.supabase.co";
-const supabaseAnonKey = "sb_publishable_l_f9mvxnd2UO2W39UuxrpQ_fQrdOIrT";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
